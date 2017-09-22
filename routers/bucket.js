53b3534977afbe6c3dd4ea47284f985d9d77edcf
Louis Lambeau <louislambeau@gmail.com>
2017-09-22 13:29:21 +0200

    Allows to enable/disable the upload page feature

--- routers/bucket.js
+++ routers/bucket.js
@@ -1,16 +1,26 @@
 'use strict';
 
+const _ = require('lodash');
 const path = require('path');
 const express = require('express');
 const fileUpload = require('./bucket/file-upload');
 const fileRetrieve = require('./bucket/file-retrieve');
 
+const DEFAULTS = {
+  uploadPage: true
+};
+
 module.exports = (config) => {
   const router = express.Router();
 
-  router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './bucket/index.html'));
-  });
+  // Merge with defaults
+  config = _.merge(DEFAULTS, config);
+
+  if (config.uploadPage) {
+    router.get('/', (req, res) => {
+      res.sendFile(path.join(__dirname, './bucket/index.html'));
+    });
+  }
 
   router.use('/', fileUpload(config));
   router.use('/', fileRetrieve(config));
