20ce296e3fddba2cebe83da50b16820f5f5a0ca2
Bernard Lambeau <blambeau@gmail.com>
2017-12-13 15:10:57 +0100

    Be slightly more verbose about the configuration loaded.

--- config/index.js
+++ config/index.js
@@ -4,11 +4,15 @@ const _ = require('lodash');
 const env = process.env.NODE_ENV || 'development';
 const fs = require('fs');
 const path = require('path');
+const logger = require('../lib/logger');
 
 let config = require('./defaults');
 
 if (fs.existsSync(path.join(__dirname, `${env}.js`))){
+  logger.info(`Loading configuration from ${env}.js`)
   config = _.merge(config, require(`./${env}.js`));
+} else {
+  logger.info(`No specific configuration for ${env}`)
 }
 
 config.get = (path) => {
