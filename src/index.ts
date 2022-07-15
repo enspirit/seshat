3a958c935f45445664b0c6160c2fd8595141f6a4
Louis Lambeau <llambeau@enspirit.be>
2022-07-15 18:52:17 +0200

    Export all lib content.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,6 @@
-import * as express from 'express';
-
-export class App {
-  public express;
-
-  constructor() {
-    this.express = express();
-    this.mountRoutes();
-  }
-
-  private mountRoutes(): void {
-    const router = express.Router();
-    router.get('/', (req, res) => {
-      res.json({
-        message: 'Hello World!',
-      });
-    });
-    this.express.use('/', router);
-  }
-}
+export * from './bucket';
+export * from './errors';
+export * from './express';
+export * from './object';
+export * from './types';
+export * from './local';
--- tests/index.spec.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import { App } from '../src';
-
-describe('App', () => {
-  it('is a class', () => {
-    expect(App).to.be.an.instanceof(Function);
-  });
-});
