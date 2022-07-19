38f3c4d89115b43b9b74ce279768b699059463dd
Louis Lambeau <llambeau@quadrabee.com>
2022-07-19 21:32:26 +0200

    read-only policy.

--- src/errors.ts
+++ src/errors.ts
@@ -1,3 +1,6 @@
 export class SeshatError extends Error {}
 export class ObjectNotFoundError extends SeshatError {}
 export class PrefixNotFoundError extends SeshatError {}
+
+export class SeshatBucketPolicyError extends SeshatError {}
+export class AccessDeniedError extends SeshatBucketPolicyError {}
---
+++ src/policies/read-only.ts
@@ -0,0 +1,22 @@
+import { AccessDeniedError } from '../errors';
+import { SeshatBucketPolicy } from '../types';
+
+const ReadOnlyPolicy: SeshatBucketPolicy = {
+
+  async get(_path): Promise<void> {
+  },
+
+  async put(_path, _meta): Promise<void> {
+    throw new AccessDeniedError('Access denied.');
+  },
+
+  async delete(_path): Promise<void> {
+    throw new AccessDeniedError('Access denied.');
+  },
+
+  async list(_prefix): Promise<void> {
+  },
+
+};
+
+export default ReadOnlyPolicy;
