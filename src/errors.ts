cdfdb8dc72fd6960686861cf2d9b3fdb7a22f60f
Louis Lambeau <llambeau@enspirit.be>
2022-07-23 19:55:57 +0200

    Linting.

--- src/errors.ts
+++ src/errors.ts
@@ -1,6 +1,7 @@
-export class SeshatError extends Error {}
-export class ObjectNotFoundError extends SeshatError {}
-export class PrefixNotFoundError extends SeshatError {}
+export class SeshatError extends Error { }
+export class ObjectNotFoundError extends SeshatError { }
+export class PrefixNotFoundError extends SeshatError { }
 
-export class SeshatBucketPolicyError extends SeshatError {}
-export class AccessDeniedError extends SeshatBucketPolicyError {}
+export class SeshatBucketPolicyError extends SeshatError { }
+export class AccessDeniedError extends SeshatBucketPolicyError { }
+
