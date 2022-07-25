9f7fe011af3f1723c2a04e1162e7ecd96b6051ca
Louis Lambeau <llambeau@enspirt.be>
2022-07-25 13:17:05 +0200

    Re-enable missing tests.

--- tests/s3/object.spec.ts
+++ tests/s3/object.spec.ts
@@ -1,58 +1,60 @@
-// import { expect } from 'chai';
-// import chai from 'chai';
-// import chaiAsPromised from 'chai-as-promised';
-// import sinon from 'sinon';
-// import sinonChai from 'sinon-chai';
-// chai.use(chaiAsPromised);
-// chai.use(sinonChai);
+import { expect } from 'chai';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+chai.use(chaiAsPromised);
+chai.use(sinonChai);
 
-// import { HeadObjectCommandOutput, S3Client } from '@aws-sdk/client-s3';
-// import S3Object from '../../src/s3/object';
+import { GetObjectCommand, HeadObjectCommandOutput, S3Client } from '@aws-sdk/client-s3';
+import S3Object from '../../src/s3/object';
 
-// describe('S3Object', () => {
+describe('S3Object', () => {
 
-//   let objectOutput: any;
-//   let s3client: Partial<S3Client>;
-//   const headObjectOutput: Partial<HeadObjectCommandOutput> = {
-//     ContentType: 'plain/text',
-//   };
-//   let object: S3Object;
+  let objectOutput: any;
+  let s3client: Partial<S3Client>;
+  const headObjectOutput: Partial<HeadObjectCommandOutput> = {
+    ContentType: 'plain/text',
+  };
+  let object: S3Object;
 
-//   beforeEach(() => {
-//     objectOutput = {
-//       createReadStream: sinon.stub(),
-//       createWriteStream: sinon.stub(),
-//     };
-//     s3client = {
-//       send: sinon.stub().returns(objectOutput),
-//     };
-//   });
+  beforeEach(() => {
+    objectOutput = {
+      createReadStream: sinon.stub(),
+      createWriteStream: sinon.stub(),
+    };
+    s3client = {
+      send: sinon.stub().returns(objectOutput),
+    };
+  });
 
-//   describe('.fromHeadOutput', () => {
+  describe('.fromHeadOutput', () => {
 
-//     it('returns a valid S3Object', () => {
-//       object = S3Object.fromHeadOutput(s3client as S3Client, 'my-bucket', 'some/key.txt', headObjectOutput as HeadObjectCommandOutput);
-//       expect(object.name).to.equal('some/key.txt');
-//       expect(object.isFile).to.equal(true);
-//     });
+    it('returns a valid S3Object', () => {
+      object = S3Object.fromHeadOutput(s3client as S3Client, 'my-bucket', 'some/key.txt', headObjectOutput as HeadObjectCommandOutput);
+      expect(object.name).to.equal('some/key.txt');
+      expect(object.isFile).to.equal(true);
+    });
 
-//   });
+  });
 
-//   describe('.getReadableStream', () => {
+  describe('.getReadableStream', () => {
 
-//     beforeEach(() => {
-//       object = S3Object.fromHeadOutput(s3client as S3Client, 'my-bucket', 'some/key.txt', headObjectOutput as HeadObjectCommandOutput);
-//     });
+    beforeEach(() => {
+      object = S3Object.fromHeadOutput(s3client as S3Client, 'my-bucket', 'some/key.txt', headObjectOutput as HeadObjectCommandOutput);
+    });
 
-//     it('uses the s3client properly', () => {
-//       object.getReadableStream();
-//       expect(s3client.send).to.be.calledOnceWith({
-//         Bucket: 'my-bucket',
-//         Key: 'some/key.txt',
-//       });
-//       expect(objectOutput.createReadStream).to.be.calledOnceWith();
-//     });
+    it('uses the s3client properly', () => {
+      object.getReadableStream();
+      expect(s3client.send).to.be.calledOnceWith(sinon.match.instanceOf(GetObjectCommand));
+      expect(s3client.send).to.be.calledOnceWith(sinon.match({
+        input: {
+          Bucket: 'my-bucket',
+          Key: 'some/key.txt',
+        }
+      }));
+    });
 
-//   });
+  });
 
-// });
+});
