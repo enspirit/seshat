2cc135379446bf94730a7952dacebcf5625ad04a
Louis Lambeau <llambeau@quadrabee.com>
2022-09-15 21:34:19 +0200

    Fix performance issue on multipart upload.

--- src/express/routers/multipart-upload.ts
+++ src/express/routers/multipart-upload.ts
@@ -24,28 +24,24 @@ export const MultipartUpload = () => (bucket: Bucket): Router => {
 
     const basePath = req.path.substring(1);
     const busboy = Busboy({ headers: req.headers });
-    const promises: Array<Promise<Object>> = [];
+    const objects: Array<ObjectMeta> = [];
 
     busboy.on('error', (error: Error) => {
       return next(error);
     });
 
-    busboy.on('file', (name, file, info) => {
+    busboy.on('file', async (name, file, info) => {
       const filepath = path.join(basePath, name);
       const metadata: ObjectMeta = {
         name: filepath,
         contentType: info.mimeType,
       };
-      const p = bucket.put(file, metadata);
-      p.catch((err) => {
-        return next(err);
-      });
-      promises.push(p);
+      const object = await bucket.put(file, metadata);
+      objects.push(object.meta);
     });
 
-    busboy.on('finish', async () => {
-      const objects = await Promise.all(promises);
-      res.status(200).send(objects.map(o => o.meta));
+    busboy.on('finish', () => {
+      res.send(objects);
     });
 
     req.pipe(busboy);
