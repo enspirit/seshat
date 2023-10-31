883d6b7be3e314f962bc110922afd0e65c0d9858
Louis Lambeau <louis.lambeau@enspirit.be>
2023-10-31 12:16:57 +0100

    Action factories.

--- src/actions/mkdir.ts
+++ src/actions/mkdir.ts
@@ -1,15 +1,22 @@
 import { Request } from 'express';
 import { Action } from '../types';
 
-export const MkdirAction: Action = {
-  name: 'mkdir',
-  run: async (req: Request): Promise<any> => {
+export const MkdirActionFactory = (): Action => {
+  return {
+    name: 'mkdir',
+    run: async (req: Request): Promise<any> => {
 
-    const path = decodeURIComponent(req.path.substring(1));
-    const bucket = req.seshat.bucket;
+      const path = decodeURIComponent(req.path.substring(1));
+      const bucket = req.seshat.bucket;
 
-    await bucket.mkdir(path);
+      await bucket.mkdir(path);
 
-    return 'ok' as any;
-  },
+      return 'ok' as any;
+    },
+  };
 };
+
+/**
+ * For backward compatibility when actions did not have parameters
+ */
+export const MkdirAction = MkdirActionFactory();
